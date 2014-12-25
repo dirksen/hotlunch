b19ba673ededefa86ef1b412d2fab90272e025e3
Dirksen <dirksen@gmail.com>
2014-12-24 23:18:53 -0500

    Convert to a nodejs/expressjs project

---
+++ public/js/data.js
@@ -0,0 +1,67 @@
+var menu={
+  "10/18/2014": {
+    "desc": "Sushi provided by ASUKA Sushi Cafe. A fruit drink will be provided with the meal",
+    "meal_type": "Sushi",
+    "options": {
+      "3": {
+        "cost": "6",
+        "option": "Avacado Roll(10 pieces)"
+      },
+      "4": {
+        "cost": "6",
+        "option": "Avacado & Cucumber Roll(10 pieces)",
+        "id": 4
+      },
+      "5": {
+        "cost": "6",
+        "option": "Cucumber roll (10 pieces)",
+        "id": 5
+      },
+      "6": {
+        "cost": "6",
+        "option": "Sweet potato noodles with vegetables and soya souce.",
+        "id": 6
+      }
+    }
+  },
+  "11/18/2014": {
+    "desc": "Sushi provided by ASUKA Sushi Cafe. A fruit drink will be provided with the meal",
+    "meal_type": "Sushi",
+    "options": {
+      "8": {
+        "cost": "6",
+        "option": "Avacado & Cucumber Roll(10 pieces)",
+        "id": 8
+      },
+      "9": {
+        "cost": "6",
+        "option": "Cucumber roll (10 pieces)",
+        "id": 9
+      },
+      "10": {
+        "cost": "6",
+        "option": "Sweet potato noodles with vegetables and soya souce.",
+        "id": 10
+      },
+      "7": {
+        "cost": "6",
+        "option": "Avacado Roll(10 pieces)"
+      }
+    }
+  },
+  "10/4/2014": {
+    "desc": "Cheese Pizza Slices are pre-ordered from Pizza Nova. Only a fruit drink will be provided with the pizza. Order now as very few extra slices will be available for sale.",
+    "meal_type": "Pizza",
+    "options": {
+      "1": {
+        "cost": "3",
+        "option": "1 slice"
+      },
+      "2": {
+        "cost": "5",
+        "option": "2 slices",
+        "id": 2
+      }
+    }
+  }
+}
