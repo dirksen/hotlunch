e82ef3d5b02d1ec8f07cf09f0f4cc8cf171c4288
Dirksen <dirksen@gmail.com>
2015-01-18 23:23:42 -0500

    New meal data

--- public/js/data.js
+++ public/js/data.js
@@ -1,65 +1,327 @@
 var menu =  [
   {
-    "date": "10/4/2013", 
-    "desc": "Cheese Pizza Slices are pre-ordered from Pizza Nova. Only a fruit drink will be provided with the pizza. Order now as very few extra slices will be available for sale.", 
-    "meal_type": "pizza", 
+    "date": "2/6/2015", 
     "options": [
       {
         "cost": "2", 
-        "option": "slice(s) of Pizza ($3 for the 1st slice, $2 per additional slice)"
-      }, 
-    ]
+        "option": "$3 for the 1st slice, $2 per additional slice"
+      }
+    ], 
+    "meal_type": "PIZZA"
   }, 
   {
-    "date": "10/18/2013", 
-    "desc": "Sushi provided by ASUKA Sushi Cafe. A fruit drink will be provided with the meal", 
-    "meal_type": "sushi", 
+    "date": "2/13/2015", 
     "options": [
       {
         "cost": "6", 
-        "option": "box(es) of Avacado Roll(10 pieces)"
+        "option": "Avacado Roll(10 pieces)"
       }, 
       {
         "cost": "6", 
-        "option": "box(es) of Avacado & Cucumber Roll(10 pieces)", 
+        "option": "Avacado & Cucumber Roll(10 pieces)", 
         "id": 4
       }, 
       {
         "cost": "6", 
-        "option": "box(es) of Cucumber roll (10 pieces)", 
+        "option": "Cucumber roll (10 pieces)", 
         "id": 5
       }, 
       {
         "cost": "6", 
-        "option": "box(es) of Sweet potato noodles with vegetables and soya souce.", 
+        "option": "Sweet potato noodles with veggies.", 
         "id": 6
       }
-    ]
+    ], 
+    "meal_type": "SUSHI"
   }, 
   {
-    "date": "11/22/2013", 
-    "desc": "Sushi provided by ASUKA Sushi Cafe. A fruit drink will be provided with the meal", 
-    "meal_type": "sushi", 
+    "date": "2/20/2015", 
     "options": [
       {
-        "cost": "6", 
-        "option": "box(es) of Avacado Roll(10 pieces)"
+        "cost": "2", 
+        "option": "$3 for the 1st slice, $2 per additional slice"
+      }
+    ], 
+    "meal_type": "PIZZA"
+  }, 
+  {
+    "date": "2/27/2015", 
+    "options": [
+      {
+        "cost": "5", 
+        "option": "Grilled Chicken on a Bun"
       }, 
       {
-        "cost": "6", 
-        "option": "box(es) of Avacado & Cucumber Roll(10 pieces)", 
-        "id": 8
-      }, 
-      {
-        "cost": "6", 
-        "option": "box(es) of Cucumber roll (10 pieces)", 
+        "cost": "5", 
+        "option": "Beef Meatballs Sub", 
         "id": 9
       }, 
       {
-        "cost": "6", 
-        "option": "box(es) of Sweet potato noodles with vegetables and soya souce.", 
+        "cost": "5", 
+        "option": "Ham & Cheese Pocket", 
         "id": 10
+      }, 
+      {
+        "cost": "5", 
+        "option": "Veggie & Cheese Pocket", 
+        "id": 11
       }
-    ]
+    ], 
+    "meal_type": "HOT SUB"
+  }, 
+  {
+    "date": "3/6/2015", 
+    "options": [
+      {
+        "cost": "2", 
+        "option": "$3 for the 1st slice, $2 per additional slice"
+      }
+    ], 
+    "meal_type": "PIZZA"
+  }, 
+  {
+    "date": "3/12/2015", 
+    "options": [
+      {
+        "cost": "5", 
+        "option": "Sweet Chicken Little"
+      }, 
+      {
+        "cost": "5", 
+        "option": "BBQ4U", 
+        "id": 14
+      }, 
+      {
+        "cost": "5", 
+        "option": "Plain Chicken", 
+        "id": 15
+      }, 
+      {
+        "cost": "5", 
+        "option": "Fee-Fi-Fo-Hummu", 
+        "id": 16
+      }
+    ], 
+    "meal_type": "PITA"
+  }, 
+  {
+    "date": "3/27/2015", 
+    "options": [
+      {
+        "cost": "2", 
+        "option": "$3 for the 1st slice, $2 per additional slice"
+      }
+    ], 
+    "meal_type": "PIZZA"
+  }, 
+  {
+    "date": "4/2/2015", 
+    "options": [
+      {
+        "cost": "6", 
+        "option": "Avacado Roll(10 pieces)"
+      }, 
+      {
+        "cost": "6", 
+        "option": "Avacado & Cucumber Roll(10 pieces)", 
+        "id": 19
+      }, 
+      {
+        "cost": "6", 
+        "option": "Cucumber roll (10 pieces)", 
+        "id": 20
+      }, 
+      {
+        "cost": "6", 
+        "option": "Sweet potato noodles with veggies.", 
+        "id": 21
+      }
+    ], 
+    "meal_type": "SUSHI"
+  }, 
+  {
+    "date": "4/10/2015", 
+    "options": [
+      {
+        "cost": "2", 
+        "option": "$3 for the 1st slice, $2 per additional slice"
+      }
+    ], 
+    "meal_type": "PIZZA"
+  }, 
+  {
+    "date": "4/17/2015", 
+    "options": [
+      {
+        "cost": "5", 
+        "option": "Grilled Chicken on a Bun"
+      }, 
+      {
+        "cost": "5", 
+        "option": "Beef Meatballs Sub", 
+        "id": 24
+      }, 
+      {
+        "cost": "5", 
+        "option": "Ham & Cheese Pocket", 
+        "id": 25
+      }, 
+      {
+        "cost": "5", 
+        "option": "Veggie & Cheese Pocket", 
+        "id": 26
+      }
+    ], 
+    "meal_type": "HOT SUB"
+  }, 
+  {
+    "date": "4/24/2015", 
+    "options": [
+      {
+        "cost": "2", 
+        "option": "$3 for the 1st slice, $2 per additional slice"
+      }
+    ], 
+    "meal_type": "PIZZA"
+  }, 
+  {
+    "date": "5/1/2015", 
+    "options": [
+      {
+        "cost": "5", 
+        "option": "Sweet Chicken Little"
+      }, 
+      {
+        "cost": "5", 
+        "option": "BBQ4U", 
+        "id": 29
+      }, 
+      {
+        "cost": "5", 
+        "option": "Plain Chicken", 
+        "id": 30
+      }, 
+      {
+        "cost": "5", 
+        "option": "Fee-Fi-Fo-Hummu", 
+        "id": 31
+      }
+    ], 
+    "meal_type": "PITA"
+  }, 
+  {
+    "date": "5/8/2015", 
+    "options": [
+      {
+        "cost": "2", 
+        "option": "$3 for the 1st slice, $2 per additional slice"
+      }
+    ], 
+    "meal_type": "PIZZA"
+  }, 
+  {
+    "date": "5/15/2015", 
+    "options": [
+      {
+        "cost": "6", 
+        "option": "Avacado Roll(10 pieces)"
+      }, 
+      {
+        "cost": "6", 
+        "option": "Avacado & Cucumber Roll(10 pieces)", 
+        "id": 34
+      }, 
+      {
+        "cost": "6", 
+        "option": "Cucumber roll (10 pieces)", 
+        "id": 35
+      }, 
+      {
+        "cost": "6", 
+        "option": "Sweet potato noodles with veggies.", 
+        "id": 36
+      }
+    ], 
+    "meal_type": "SUSHI"
+  }, 
+  {
+    "date": "5/22/2015", 
+    "options": [
+      {
+        "cost": "2", 
+        "option": "$3 for the 1st slice, $2 per additional slice"
+      }
+    ], 
+    "meal_type": "PIZZA"
+  }, 
+  {
+    "date": "5/29/2015", 
+    "options": [
+      {
+        "cost": "5", 
+        "option": "Grilled Chicken on a Bun"
+      }, 
+      {
+        "cost": "5", 
+        "option": "Beef Meatballs Sub", 
+        "id": 39
+      }, 
+      {
+        "cost": "5", 
+        "option": "Ham & Cheese Pocket", 
+        "id": 40
+      }, 
+      {
+        "cost": "5", 
+        "option": "Veggie & Cheese Pocket", 
+        "id": 41
+      }
+    ], 
+    "meal_type": "HOT SUB"
+  }, 
+  {
+    "date": "6/4/2015", 
+    "options": [
+      {
+        "cost": "2", 
+        "option": "$3 for the 1st slice, $2 per additional slice"
+      }
+    ], 
+    "meal_type": "PIZZA"
+  }, 
+  {
+    "date": "6/12/2015", 
+    "options": [
+      {
+        "cost": "5", 
+        "option": "Sweet Chicken Little"
+      }, 
+      {
+        "cost": "5", 
+        "option": "BBQ4U", 
+        "id": 44
+      }, 
+      {
+        "cost": "5", 
+        "option": "Plain Chicken", 
+        "id": 45
+      }, 
+      {
+        "cost": "5", 
+        "option": "Fee-Fi-Fo-Hummu", 
+        "id": 46
+      }
+    ], 
+    "meal_type": "PITA"
+  }, 
+  {
+    "date": "6/19/2015", 
+    "options": [
+      {
+        "cost": "2", 
+        "option": "$3 for the 1st slice, $2 per additional slice"
+      }
+    ], 
+    "meal_type": "PIZZA"
   }
 ]
