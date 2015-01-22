6019181a1e4c33dc81c1747f5e593d8c7bd064d1
Dirksen <dirksen@knexxlocal.com>
2015-01-21 22:30:36 -0500

    Fix meal type on import

--- parse.tsv.py
+++ parse.tsv.py
@@ -9,7 +9,7 @@ for i, ln in enumerate(sys.stdin):
         cost = cost[1:] if cost.startswith('$') else cost
         if date:
             xdate = date
-            menu.append(dict(date=date, meal_type=meal_type, options=[dict(option=option, cost=cost)]))
+            menu.append(dict(date=date, meal_type=meal_type.replace(' DAY', ''), options=[dict(option=option, cost=cost)]))
         else:
             menu[-1]['options'].append(dict(id=i, option=option, cost=cost))
 print 'var menu = ', json.dumps(menu, indent=2)
